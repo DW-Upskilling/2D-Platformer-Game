bc431a99e2c2a3697a100b68211e85eb7b0ece34
Developers@Work <DevelopersWork@users.noreply.github.com>
2023-04-28 23:08:32 +0530

    Chapter 4 - Mini Assignment 1 - To the Next level

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (
+            collision.gameObject.CompareTag("Player") &&
+            collision.gameObject.GetComponent<PlayerController>() != null
+           ) {
+            Debug.Log("Khatam Ho Gayaaa. Only once faskkk");
+            Debug.Log("Level Complete. Start the next level");
+        }
+    }
+
+}
